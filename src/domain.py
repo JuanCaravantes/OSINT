8ca763c9acceff7eb382ba2dc797f142f1664028
JuanCaravantes <juan.caravantes@alu.uclm.es>
2022-04-07 18:13:22 +0200

    Src
    Src con el script de busqueda de dominio. Busca en los tres tipos de servidores de DNS para buscar posibles coincidencias

---
+++ src/domain.py
@@ -0,0 +1,35 @@
+from dns import *
+from enum import Enum
+
+class DNSDomain():      
+
+    def comprobar_dominio(nombre):
+        try:
+            ans = resolver.resolve(nombre,"A")
+            for i in ans.response.answer:
+                print (i.to_text())
+        except:
+            print("El nombre de dominio "+nombre+ "no se encontro en el dominio A")
+        try:
+            ans = resolver.resolve(nombre,"MX")
+            for i in ans.response.answer:
+                print (i.to_text())
+        except:
+            print("El nombre de dominio "+nombre+ "no se encontro en el dominio MX")
+          
+        try:
+            ans = resolver.resolve(nombre,"NS")
+            for i in ans.response.answer:
+                print (i.to_text())
+        except:
+            print("El nombre de dominio "+nombre+ "no se encontro en el dominio NS")
+
+        try:
+            ans = resolver.resolve(nombre,"CNAME")
+            for i in ans.response.answer:
+                print (i.to_text())
+        except:
+            print("El nombre de dominio "+nombre+ "no se encontro en el dominio CNAME")
+
+if __name__ == "__main__":
+    DNSDomain.comprobar_dominio("kali.com")
